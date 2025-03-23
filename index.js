16f9d8d1502c8704a790337623296db26e325cd0
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 15:49:02 +0300

    commit my first push

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const tutorials = [
-  "what is JavaScript?",
-  "understanding the this keyword",
-  "introduction to node.js",
-  "callbacks and promises",
-  "the event loop in depth"
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 function titleCased() {
