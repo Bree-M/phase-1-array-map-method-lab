753f15deb63e66673ceba044c914b503479e91f4
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 16:20:00 +0300

    another update

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const tutorials = [
-      "WhatIsTheDifferenceBetweenEventCapturingAndBubbling?",
-       "What Does The This Keyword Mean?",
-       "What Is The Constructor OO Pattern?",
-       "Implementing Blockchain Web API",
-       "The Test Driven Development Workflow",
-       "What Is NaN And How Can We Check For It",
-       "What Is The Difference Between StopPropagation And PreventDefault?",
-        "Immutable State And Pure Functions",
-        "What Is The Difference Between == And ===?",
-        "What Is The Difference Between Event Capturing And Bubbling?",
-        "What Is JSONP?"
-        
+  "what does the this keyword mean?",
+  "what is the constructor oo pattern?",
+  "implementing blockchain web api",
+  "the test driven development workflow",
+  "what is nan and how can we check for it",
+  "what is the difference between stopPropagation and preventDefault?",
+  "immutable state and pure functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is jsonp?"
 ];
 
 const titleCased = () => {
-   return tutorials.map(tutorial => {
-   return tutorial
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join("");
+  return tutorials.map(tutorial => 
+    tutorial
+      .split(" ") 
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
+      .join(" ") 
+  );
+};
 
-  });
-}
- console.log(titleCased());
+console.log(titleCased());
