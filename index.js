9b930c789847cbdd8a325b753574da87c094c674
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 16:27:20 +0300

    learning update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const tutorials = [
-  "What Is The Constructor OO Pattern?",
-        "Implementing Blockchain Web API",
+  "What Does The This Keyword Mean?",
+         "What Is The Constructor OO Pattern?",
+         "Implementing Blockchain Web API",
          "The Test Driven Development Workflow",
-  "What Is NaN And How Can We Check For It",
+         "What Is NaN And How Can We Check For It",
          "What Is The Difference Between StopPropagation And PreventDefault?",
          "Immutable State And Pure Functions",
          "What Is The Difference Between == And ===?",
