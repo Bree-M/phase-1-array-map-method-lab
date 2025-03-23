ee24ae59be1d7c6c9e1e662201150bb8efebe9ab
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 16:13:25 +0300

    another update to my commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const tutorials = [
-  
-    "What Does The This Keyword Mean?",
-    "What Is The Constructor OO Pattern?",
-    "Implementing Blockchain Web API",
-    "The Test Driven Development Workflow",
-    "What Is NaN And How Can We Check For It",
-    "What Is The Difference Between StopPropagation And PreventDefault?",
-    "Immutable State And Pure Functions",
-    "What Is The Difference Between == And ===?",
-    "What Is The Difference Between Event Capturing And Bubbling?",
-    "What Is JSONP?"
-  
-  
+      "WhatIsTheDifferenceBetweenEventCapturingAndBubbling?",
+       "What Does The This Keyword Mean?",
+       "What Is The Constructor OO Pattern?",
+       "Implementing Blockchain Web API",
+       "The Test Driven Development Workflow",
+       "What Is NaN And How Can We Check For It",
+       "What Is The Difference Between StopPropagation And PreventDefault?",
+        "Immutable State And Pure Functions",
+        "What Is The Difference Between == And ===?",
+        "What Is The Difference Between Event Capturing And Bubbling?",
+        "What Is JSONP?"
         
 ];
 
