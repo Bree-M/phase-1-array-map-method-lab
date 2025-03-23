f9006a890a25c6edb3f3c83131a0abdab64ecb04
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 05:30:55 +0300

    almost done

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what is JavaScript?",
+  "understanding the this keyword",
+  "introduction to node.js",
+  "callbacks and promises",
+  "the event loop in depth"
 ];
 
 function titleCased() {
