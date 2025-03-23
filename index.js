d47f09b51d92c613caf250d029f75f6504981b05
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 16:08:07 +0300

    second try

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const tutorials = [
-  "what does the this keyword mean?",
-  "what is the constructor oo pattern?",
-  "implementing blockchain web api",
-  "the test driven development workflow",
-  "what is nan and how can we check for it",
-  "what is the difference between stopPropagation and preventDefault?",
-  "immutable state and pure functions",
-  "what is the difference between == and ===?",
-  "what is the difference between event capturing and bubbling?",
-  "what is jsonp?"
+      "WhatIsTheDifferenceBetweenEventCapturingAndBubbling?",
+       "What Does The This Keyword Mean?",
+       "What Is The Constructor OO Pattern?",
+       "Implementing Blockchain Web API",
+       "The Test Driven Development Workflow",
+       "What Is NaN And How Can We Check For It",
+       "What Is The Difference Between StopPropagation And PreventDefault?",
+        "Immutable State And Pure Functions",
+        "What Is The Difference Between == And ===?",
+        "What Is The Difference Between Event Capturing And Bubbling?",
+        "What Is JSONP?"
+        
 ];
 
 const titleCased = () => {
